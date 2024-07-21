cd34ed4a11aa3b9abd7a2236b4bfe866457e0ad7
Nurlan199206 <user_1771@mail.ru>
2024-07-21 17:39:49 +0500

    add new server and add the disk creation

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "proxmox_vm_qemu" "srv-01" {
     cpu         = "host"
     memory      = 2048
 
+
     network {
       bridge   = "vmbr0"
       model    = "virtio"
@@ -19,4 +20,47 @@ resource "proxmox_vm_qemu" "srv-01" {
     nameserver = "192.168.1.1" 
     ciuser     = "nurlan"
     cipassword = "123"
+}
+
+
+resource "proxmox_vm_qemu" "srv-02" {
+    name        = "srv-02"
+    desc        = "Ubuntu Server"
+    vmid        = "102"
+    target_node = "proxmox"
+    cores       = 2
+    sockets     = 1
+    cpu         = "host"
+    memory      = 2048
+    bootdisk    =  "scsi0"
+    scsihw      = "lsi"
+     
+
+disks {
+  scsi {
+    scsi0 {
+      cdrom {
+        iso = "local:iso/ubuntu-22.04.4-live-server-amd64.iso"
+      }
+      }
+  scsi1 {
+    disk {
+        storage = "local-lvm"
+        size = "20G"
+      }
+    }
+  }
+}
+
+network {
+    bridge   = "vmbr0"
+    model    = "virtio"
+    firewall = false
+    }
+   
+    os_type    = "cloud-init"
+    ipconfig0  = "ip=192.168.1.161/24,gw=192.168.1.1"
+    nameserver = "192.168.1.1" 
+    ciuser     = "nurlan"
+    cipassword = "123"  
 }
